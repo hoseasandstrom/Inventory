ebaf12ac89df58c6b5b1dd59ebcb3bdd9426b1cd
hoseasandstrom <Lakyn4ever>
2016-07-11 12:46:45 -0400

    Added factory method that creates the objects based on the category name.

--- src/com/studenttheironyard/Main.java
+++ src/com/studenttheironyard/Main.java
@@ -6,46 +6,13 @@ import java.util.Scanner;
 
 
 public class Main {
-    static InventoryItem createItem(String name, int qty, String category) {
-        return null;
-    }
-
-    public static void createItem(Scanner scanner, ArrayList<InventoryItem> items) {
-        System.out.println("Enter the name of the beer:");
-        String text = scanner.nextLine();
-        InventoryItem item = new InventoryItem();
-        items.add(item);
-        }
-
-
-
-    public static void removeItem(Scanner scannner, ArrayList<InventoryItem> items) {
-        System.out.println("Enter the line of the inventory item you wish to remove:");
-        String numStr = scannner.nextLine();
-        Integer num = Integer.valueOf(numStr);
-        try {
-            items.remove(num - 1);
-            }
-        catch (NumberFormatException e) {
-            System.out.println("That is not a valid number!");
-        }
-        catch (ArrayIndexOutOfBoundsException e) {
-            System.out.println("That number isn't valid!");
-
-        }
-    }
-    public static void listItem(ArrayList<InventoryItem> items) {
-        System.out.println("Here is your current inventory:");
-        int i = 1;
-        for (InventoryItem inventoryItem : items) {
-            System.out.println(i + ". " + "[" + inventoryItem.qty + "]" + inventoryItem.name);
-            i++;
-        }
-    }
+    public static ArrayList<InventoryItem> itemList = new ArrayList<>();
 
         public static void main(String[] args)  {
-            ArrayList<InventoryItem> items = new ArrayList<>();
             Scanner scanner = new Scanner(System.in);
+            for (InventoryItem item : itemList) {
+                System.out.printf("%s. [%s] %s [%s]\n" , (itemList.indexOf(item) + 1),  item.qty, item.name, item.category);
+            }
 
             while (true) {
                 System.out.println("1. Enter the beer into our inventory:");
@@ -54,26 +21,85 @@ public class Main {
 
 
                 String option = scanner.nextLine();
-                switch (option) {
-                    case "1":
-                        //create new item
-                        createItem(scanner, items);
-                        break;
+                if (option.equalsIgnoreCase("1")) {
 
-                    case "2":
-                        //toggle item
-                        removeItem(scanner, items);
-                        break;
+                    System.out.println("Enter in the new beer:");
+                    String newItem = scanner.nextLine();
+                    System.out.println("Enter in the quantity:");
+                    String newQty = scanner.nextLine();
 
-                    case "3":
-                        listItem(items);
-                        break;
+                    int qty = Integer.valueOf(newQty);
+                    if(qty > 1){
+                        String temp = newItem.concat("s");
+                        newItem = temp;
 
-                    default:
-                        System.out.println("Invalid option");
-                }
             }
 
+            System.out.println("Select the category of the new beer.");
+            System.out.println("[1.] IPA");
+            System.out.println("[2.] Stout");
+            System.out.println("[3.] Porter");
+            System.out.println("[4.] Ale");
+            System.out.println("[5.] Lager");
+            String newCategory = scanner.nextLine();
+            InventoryItem item = createItem(newItem, qty , newCategory);
+            if(item != null){
+                itemList.add(item);
+            }
+            System.out.printf("You have added %s %s to your inventory.\n", newQty, newItem);
+
+        }
+    else if(option.equalsIgnoreCase("2")){
+
+        System.out.println("Please select the item number to delete");
+        for (InventoryItem item : itemList) {
+            System.out.printf("%s. [ %s ] %s [%s]\n", (itemList.indexOf(item) +1), item.qty, item.name, item.category);
+        }
+        String choice = scanner.nextLine();
+        int num = Integer.valueOf(choice);
+        itemList.remove(num-1);
+        System.out.println();
+    }
+    else if(option.equalsIgnoreCase("3")){
+
+        System.out.println("Please select which item's quantity you wish to update");
+        for (InventoryItem item : itemList) {
+            System.out.printf("%s . [ %s ] %s [%s]\n", (itemList.indexOf(item) +1), item.qty, item.name, item.category);
+        }
+        String choice = scanner.nextLine();
+        int num = Integer.valueOf(choice);
+        InventoryItem item = itemList.get(num -1);
+
+        System.out.printf("Please enter the new number of %s in your inventory.\n", item.name);
+        String newItemAmount = scanner.nextLine();
+        int amm = Integer.valueOf(newItemAmount);
+        item.qty = amm;
+        System.out.printf("%s. You now have [%s] %s\n", (itemList.indexOf(item)+ 1), amm, item.name);
+    }
+    else if(option.equalsIgnoreCase("4")){
+        for (InventoryItem item : itemList) {
+            System.out.printf("%s . [%s] %s [%s]\n", (itemList.indexOf(item) +1), item.qty, item.name, item.category);
         }
     }
+}
+}
+public static InventoryItem createItem(String newItem, int newQty, String newCategory) {
+
+        switch (newCategory){
+        case "1":
+        return new IPA(newItem, newQty);
+        case "2":
+        return new Stout(newItem, newQty);
+        case "3":
+        return new Porter(newItem, newQty);
+        case "4":
+        return new Ale(newItem, newQty);
+        case "5":
+        return new Lager(newItem, newQty);
+default:
+        System.out.println("Please choose different number");
+        }
+        return null;
+        }
+}
 
